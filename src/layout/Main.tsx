e87dbddb034cd8970230e5c0cb596ac7bacb25f3
daehyun <detail54@nate.com>
2022-07-26 14:19:57 +0900

    head수정

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -7,33 +7,28 @@ import MainEl from './Main.styles'
 const Main: NextPage<AppProps> = ({ Component, pageProps, router }) => {
   const domain = `https://도메인명`
   const currentUrl = `${domain}/${router.asPath}`
-  const pageName = router.pathname === '/' ? '' : router.pathname
+  const pageName =
+    router.pathname === '/' ? '' : router.pathname.replace('/', '')
 
   return (
     <>
       <Head>
         <link rel='canonical' href={currentUrl} />
-        <title>{`next react query boilerplate${pageName}`}</title>
-        <meta name='description' content='__next react query boilerplate' />
-        <meta property='og:title' content='__next react query boilerplate' />
+        <title>{`가계부 / ${pageName}`}</title>
+        <meta name='description' content='가계부' />
+        <meta property='og:title' content='가계부' />
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1, viewport-fit=cover'
         />
-        <meta
-          property='og:description'
-          content='__next react query boilerplate'
-        />
+        <meta property='og:description' content='가계부' />
         <meta property='og:url' content={currentUrl} />
         {/* <meta
           property='og:image'
           content={`${domain}/opengraph.png?${(Math.random() * 7).toString(7)}`}
         /> */}
         <meta property='og:type' content='website' />
-        <meta
-          property='og:site_name'
-          content='__next react query boilerplate'
-        />
+        <meta property='og:site_name' content='가계부' />
       </Head>
       <MainEl>
         <Component {...pageProps} />
