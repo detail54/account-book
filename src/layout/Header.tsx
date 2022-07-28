039e1cc899eadd2c103b31e5081326d003043ecb
daehyun <detail54@nate.com>
2022-07-28 10:04:08 +0900

    상단 텍스트 수정

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -19,9 +19,7 @@ const Header: React.FC<IProps> = ({ changeThemeButtonText, onChangeTheme }) => {
 
   return (
     <HeaderEl>
-      <UserInfo>
-        {!session ? '로그인 해주세요.' : `${session.user.name} 님`}
-      </UserInfo>
+      <UserInfo>{!session ? '' : `${session.user.name} 님`}</UserInfo>
       <Buttons>
         <Button
           buttonStyle='RoundButton'
