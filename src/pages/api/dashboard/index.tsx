8d3f0a2850e70a31e6fb7e6258897ed4647e4e71
daehyun <detail54@nate.com>
2022-09-20 11:09:35 +0900

    테스트 코드추가

--- src/pages/api/dashboard/index.tsx
+++ src/pages/api/dashboard/index.tsx
@@ -4,146 +4,144 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from 'utils/prismaClient'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('hihihihihihi')
-  // const { body, headers, query, method } = req
-  // const token = getCookie('next-auth.session-token', { req, res }) as string
-  // const date = query.date as string
+  const { body, headers, query, method } = req
+  const token = getCookie('next-auth.session-token', { req, res }) as string
+  const date = query.date as string
 
-  // if (!date) {
-  //   res.status(500).json(new Error('not found "date" param'))
-  // }
+  if (!date) {
+    res.status(500).json(new Error('not found "date" param'))
+  }
 
-  // if (!token) {
-  //   return
-  // }
+  if (!token) {
+    return
+  }
 
-  // const base64Payload = token.split('.')[1]
-  // const payload = Buffer.from(base64Payload, 'base64')
-  // const userId = JSON.parse(payload.toString()).id
+  const base64Payload = token.split('.')[1]
+  const payload = Buffer.from(base64Payload, 'base64')
+  const userId = JSON.parse(payload.toString()).id
 
-  // const apiMethod = method || 'GET'
+  const apiMethod = method || 'GET'
 
-  // const handler: { [key: string]: () => void } = {
-  //   GET: async () => {
-  //     try {
-  //       const [year, month] = date.split('-')
-  //       const lastDate = new Date(
-  //         Number(year),
-  //         Number(month),
-  //         0,
-  //         23,
-  //         59,
-  //         59,
-  //         999,
-  //       )
-  //       const firstDate = new Date(
-  //         Number(year),
-  //         Number(month) - 1,
-  //         1,
-  //         0,
-  //         0,
-  //         0,
-  //         0,
-  //       )
+  const handler: { [key: string]: () => void } = {
+    GET: async () => {
+      try {
+        const [year, month] = date.split('-')
+        const lastDate = new Date(
+          Number(year),
+          Number(month),
+          0,
+          23,
+          59,
+          59,
+          999,
+        )
+        const firstDate = new Date(
+          Number(year),
+          Number(month) - 1,
+          1,
+          0,
+          0,
+          0,
+          0,
+        )
 
-  //       const accountDatas = await prisma.account.findMany({
-  //         where: {
-  //           id: userId,
-  //           paymentDt: {
-  //             gte: firstDate,
-  //             lte: lastDate,
-  //           },
-  //         },
-  //         select: {
-  //           id: true,
-  //           user: true,
-  //           store: true,
-  //           category: true,
-  //           amount: true,
-  //           memo: true,
-  //           regDt: true,
-  //           updatedDt: true,
-  //           paymentDt: true,
-  //         },
-  //       })
+        const accountDatas = await prisma.account.findMany({
+          where: {
+            id: userId,
+            paymentDt: {
+              gte: firstDate,
+              lte: lastDate,
+            },
+          },
+          select: {
+            id: true,
+            user: true,
+            store: true,
+            category: true,
+            amount: true,
+            memo: true,
+            regDt: true,
+            updatedDt: true,
+            paymentDt: true,
+          },
+        })
 
-  //       const incomeDatas = await prisma.income.findMany({
-  //         where: {
-  //           userId,
-  //           incomeDt: {
-  //             gte: firstDate,
-  //             lte: lastDate,
-  //           },
-  //         },
-  //         select: {
-  //           id: true,
-  //           user: true,
-  //           amount: true,
-  //           memo: true,
-  //           regDt: true,
-  //           updatedDt: true,
-  //           incomeDt: true,
-  //         },
-  //       })
+        const incomeDatas = await prisma.income.findMany({
+          where: {
+            userId,
+            incomeDt: {
+              gte: firstDate,
+              lte: lastDate,
+            },
+          },
+          select: {
+            id: true,
+            user: true,
+            amount: true,
+            memo: true,
+            regDt: true,
+            updatedDt: true,
+            incomeDt: true,
+          },
+        })
 
-  //       const initResultData: IDashBoard = {
-  //         list: [],
-  //         totalIncome: 0,
-  //         totalExpenditure: 0,
-  //       }
-  //       const resultData: IDashBoard = Array.from(
-  //         { length: lastDate.getDate() },
-  //         (v, i) => i + 1,
-  //       ).reduce((acc, cur) => {
-  //         const data = accountDatas.filter(
-  //           (account) => account.paymentDt.getDate() === cur,
-  //         )
+        const initResultData: IDashBoard = {
+          list: [],
+          totalIncome: 0,
+          totalExpenditure: 0,
+        }
+        const resultData: IDashBoard = Array.from(
+          { length: lastDate.getDate() },
+          (v, i) => i + 1,
+        ).reduce((acc, cur) => {
+          const data = accountDatas.filter(
+            (account) => account.paymentDt.getDate() === cur,
+          )
 
-  //         const returnDate = `${date}-${cur < 10 ? `0${cur}` : cur}`
-  //         if (data) {
-  //           const totalIncome = incomeDatas
-  //             .filter((income) => income.incomeDt.getDate() === cur)
-  //             .reduce((_acc, _cur) => {
-  //               return _acc + _cur.amount
-  //             }, 0)
-  //           const totalExpenditure = data.reduce((_acc, _cur) => {
-  //             return _acc + _cur.amount
-  //           }, 0)
+          const returnDate = `${date}-${cur < 10 ? `0${cur}` : cur}`
+          if (data) {
+            const totalIncome = incomeDatas
+              .filter((income) => income.incomeDt.getDate() === cur)
+              .reduce((_acc, _cur) => {
+                return _acc + _cur.amount
+              }, 0)
+            const totalExpenditure = data.reduce((_acc, _cur) => {
+              return _acc + _cur.amount
+            }, 0)
 
-  //           return {
-  //             ...acc,
-  //             list: [
-  //               ...acc.list,
-  //               {
-  //                 date: returnDate,
-  //                 income: totalIncome,
-  //                 expenditure: totalExpenditure,
-  //               },
-  //             ],
-  //             totalIncome: acc.totalIncome + totalIncome,
-  //             totalExpenditure: acc.totalExpenditure + totalExpenditure,
-  //           }
-  //         }
+            return {
+              ...acc,
+              list: [
+                ...acc.list,
+                {
+                  date: returnDate,
+                  income: totalIncome,
+                  expenditure: totalExpenditure,
+                },
+              ],
+              totalIncome: acc.totalIncome + totalIncome,
+              totalExpenditure: acc.totalExpenditure + totalExpenditure,
+            }
+          }
 
-  //         return {
-  //           ...acc,
-  //           list: [
-  //             ...acc.list,
-  //             { date: returnDate, income: 0, expenditure: 0 },
-  //           ],
-  //         }
-  //       }, initResultData)
-  //       const result = {
-  //         test: 'test',
-  //       }
+          return {
+            ...acc,
+            list: [
+              ...acc.list,
+              { date: returnDate, income: 0, expenditure: 0 },
+            ],
+          }
+        }, initResultData)
+        const result = {
+          test: 'test',
+        }
 
-  //       res.status(200).json(result)
-  //     } catch (e) {
-  //       res.status(500)
-  //     }
-  //   },
-  // }
+        res.json(result)
+      } catch (e) {
+        res.status(500)
+      }
+    },
+  }
 
-  // handler[apiMethod]()
-  res.json({ message: 'okokok' })
+  handler[apiMethod]()
 }
